333d112b3e08187b633ff108885d370b492df3db
Scott <stsmith@callutheran.edu>
2019-08-30 09:24:03 -0700

    vue version

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	
+	<main class="container">
+		<div class="row">
+			<div class="col-sm-12 main-content">
+				<h1 class="section-title">Image Resizer</h1>
+
+				
+
+			</div>
+		</div>
+	</main>
+
+</template>
+
+<script>
+
+	import Vue from "vue";
+
+	export default {
+		data(){
+			return {
+				test: 'hi'
+			}
+		}
+	}
+
+</script>
