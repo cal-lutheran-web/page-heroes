003579df87d36aea2f19e512ec72e2665f6b6bb4
Scott <stsmith@callutheran.edu>
2019-08-30 13:36:06 -0700

    vue version with advanced-cropper

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,46 @@
 	
 	<main class="container">
 		<div class="row">
-			<div class="col-sm-12 main-content">
+			<section class="col-sm-12">
 				<h1 class="section-title">Image Resizer</h1>
 
-				
+				<h2>Upload an Image</h2>
 
-			</div>
+				<label for="upload-select">Select a file to upload</label>
+                <input type="file" class="cropit-image-input" id="upload-select" @change="handleFileChange" />
+
+			</section>
+			<section class="col-sm-12">
+				<h2>Choose a Style of Image</h2>
+
+				<select v-model="style">
+					<option value=""></option>
+					<option value="profile">Profile</option>
+				</select>
+
+			</section>
+			<section class="col-sm-12">
+				<h2>Make Edits</h2>
+
+				<Cropper
+					classname="cropper"
+					:src="this.img"
+					:stencilProps="{
+						aspectRatio: 2/3,
+					}"
+					@change="cropChange"
+				></Cropper>
+
+			</section>
+			<section class="col-sm-12">
+				<h2>Preview</h2>
+
+				<ShortItem :img="this.image" v-if="this.style == 'profile'" />
+
+			</section>
+			<section v-if="this.image" class="col-sm-12">
+				<a :href="this.image" download="image.jpg" class="btn green btn-large">Download</a>
+			</section>
 		</div>
 	</main>
 
@@ -16,13 +50,50 @@
 <script>
 
 	import Vue from "vue";
+	import { Cropper } from 'vue-advanced-cropper';
+	import ShortItem from './ShortItem.vue';
 
 	export default {
 		data(){
 			return {
-				test: 'hi'
+				img: '',
+				style: '',
+				image: '',
+				coordinates: {
+					width: 0,
+					height: 0,
+					left: 0,
+					top: 0
+				},
 			}
+		},
+		components: {
+			Cropper,
+			ShortItem
+		},
+		methods: {
+			handleFileChange(e){
+				var reader = new FileReader();
+				reader.onload = (e) => {
+					this.img = e.target.result;
+				}
+				reader.readAsDataURL(e.target.files[0]);
+			},
+			cropChange({coordinates, canvas}) {
+				this.coordinates = coordinates;
+				this.image = canvas.toDataURL();
+			},
 		}
 	}
 
 </script>
+
+
+<style scoped>
+
+	.cropper {
+		max-width: 400px;
+		margin: auto;
+	}
+
+</style>
---
+++ src/ShortItem.vue
@@ -0,0 +1,27 @@
+<template>
+
+	<div class="short-item">
+		<div class="short-item-aside" id="profile-preview">
+			<!-- <i id="placeholder-spinner" class="icon-loading loading-spinner hide"></i> -->
+			<img :src="img" />
+		</div>
+		<div class="short-item-content">
+		<header>Department Staff</header>
+		<p><i class="icon-user"></i> <em>Job Title</em><br>
+		<i class="icon-mail"></i> <a href="">email@callutheran.edu</a><br>
+		<i class="icon-phone"></i> 555-1234</p>
+		</div>
+	</div>
+
+</template>
+
+<script>
+
+	import Vue from "vue";
+
+	export default {
+		name: 'ShortItem',
+		props: ['img'],
+	}
+
+</script>
