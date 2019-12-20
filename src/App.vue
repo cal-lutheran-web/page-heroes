8e22a9bd75058eb9143eb902df69aa7718403d90
Scott Smith <stsmith@callutheran.edu>
2019-12-20 11:15:14 -0800

    crop to large hero size

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,9 @@
   <main class="container">
     <div class="row">
       <section class="col-sm-12">
-        <h1 class="section-title">Image Resizer</h1>
+        <h1 class="section-title">Page Heroes</h1>
       </section>
       <section class="col-sm-12">
-        <h2>Choose a Style of Image</h2>
-
-        <select v-model="style">
-          <option value></option>
-          <option value="profile">Profile</option>
-          <option value="featured-col">Featured Column</option>
-        </select>
-      </section>
-      <section class="col-sm-12" v-if="this.style">
         <h2>Upload an Image</h2>
 
         <label for="upload-select">Select a file to upload</label>
@@ -35,12 +26,8 @@
       <section class="col-sm-12" v-if="this.newImage">
         <h2>Preview</h2>
 
-        <div id="preview-area" v-if="this.style === 'profile'">
-          <ShortItem :img="this.newImage" />
-        </div>
-        <div id="preview-area" v-else-if="this.style === 'featured-col'">
-          <FeaturedCol :img="this.newImage" />
-        </div>
+        <img :src="this.newImage" alt="feature" width="1280" height="342" />
+
       </section>
       <section v-if="this.newImage" class="col-sm-12">
         <a :href="this.newImage" :download="this.fileName" class="btn green btn-large">Download</a>
@@ -52,8 +39,6 @@
 <script>
 import Vue from "vue";
 import { Cropper } from "vue-advanced-cropper";
-import ShortItem from "./ShortItem.vue";
-import FeaturedCol from "./FeaturedCol.vue";
 const Pica = require("pica")();
 
 export default {
@@ -63,17 +48,14 @@ export default {
       style: "",
       newImage: "",
       fileName: "",
-      imageTypes: ["profile", "featured-col"],
-      aspectRatio: "",
-      canvasWidth: "",
-      canvasHeight: "",
+      aspectRatio: 640/171,
+      canvasWidth: 1280,
+      canvasHeight: 342,
       refresh: 0
     };
   },
   components: {
-    Cropper,
-    ShortItem,
-    FeaturedCol
+    Cropper
   },
   methods: {
     handleFileChange(e) {
@@ -107,23 +89,6 @@ export default {
             $this.newImage = newFile.result;
           };
         });
-    }
-  },
-  watch: {
-    style(data) {
-      if (data == "profile") {
-        Vue.set(this, "canvasWidth", 200);
-        Vue.set(this, "canvasHeight", 300);
-        Vue.set(this, "aspectRatio", 2 / 3);
-      }
-
-      if (data == "featured-col") {
-        Vue.set(this, "canvasWidth", 556);
-        Vue.set(this, "canvasHeight", 312);
-        Vue.set(this, "aspectRatio", 16 / 9);
-      }
-
-      this.resfresh = 1;
     }
   }
 };
