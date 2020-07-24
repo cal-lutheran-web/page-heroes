031a44f16cf5e34627583ddc86aa20a169edaa36
Scott Smith <stsmith@callutheran.edu>
2020-07-24 15:55:27 -0700

    correctly use page-hero styles from main site

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,43 @@
 <template>
-  <main class="container">
-    <div class="row">
-      <section class="col-sm-12">
-        <h1 class="section-title">Page Heroes</h1>
-      </section>
-      <section class="col-sm-12">
-        <h2>Upload an Image</h2>
+  <main>
+    <div class="container">
+      <div class="row">
+        <section class="col-sm-12">
+          <h1 class="section-title">Page Heroes</h1>
+        </section>
+        <section class="col-sm-12">
+          <h2>Upload an Image</h2>
 
-        <label for="upload-select">Select a file to upload</label>
-        <input type="file" class="cropit-image-input" id="upload-select" @change="handleFileChange" />
-      </section>
+          <label for="upload-select">Select a file to upload</label>
+          <input type="file" class="cropit-image-input" id="upload-select" @change="handleFileChange" />
+        </section>
 
-      <section class="col-sm-12" v-if="this.oldImage">
-        <h2>Make Edits</h2>
+        <section class="col-sm-12" v-if="this.oldImage">
+          <h2>Make Edits</h2>
 
-        <Cropper
-          ref="cropImg"
-          class="cropper"
-          :src="this.oldImage"
-          :stencilProps="{ aspectRatio: this.aspectRatio }"
-          @change="cropChange"
-          :key="this.refresh"
-        ></Cropper>
-      </section>
-      <section class="col-sm-12" v-if="this.newImage">
-        <h2>Preview</h2>
+          <Cropper
+            ref="cropImg"
+            class="cropper"
+            :src="this.oldImage"
+            :stencilProps="{ aspectRatio: this.aspectRatio }"
+            @change="cropChange"
+            :key="this.refresh"
+          ></Cropper>
+        </section>
 
-        <img :src="this.newImage" alt="feature" width="1280" height="342" />
-
+        <section v-if="this.newImage" class="col-sm-12">
+          <a :href="this.newImage" :download="this.fileName" class="btn green btn-large">Download</a>
+        </section>
+      </div>
+</div>
+  <div class="container-fluid">
+      <section class="page-hero tall" :style="{ 'background-image': 'url('+this.newImage+')' }">
+        <div class="overlay-low"></div>
+        <div class="page-hero-heading-wrapper">
+          <h1><a href="#">This is a Page Hero</a></h1>
+        </div>
       </section>
-      <section v-if="this.newImage" class="col-sm-12">
-        <a :href="this.newImage" :download="this.fileName" class="btn green btn-large">Download</a>
-      </section>
-    </div>
-  </main>
+ </main>
 </template>
 
 <script>
@@ -96,11 +100,25 @@ export default {
 
 
 <style scoped>
+body {
+  padding:0;
+  margin: 0;
+}
 .cropper {
   max-width: 40%;
   margin: auto;
 }
 main {
   margin-bottom: 8rem;
+}
+
+
+.container-fluid {
+  padding: 0;
+  margin-top: 3rem;
+}
+
+.page-hero {
+  background-position: center bottom;
 }
 </style>
